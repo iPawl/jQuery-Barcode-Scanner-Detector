cf6c4f083ea4992d69cc3c33de3ceca107ab1d34
Pavel Fomichev <pavel.fomichev@socialsys.ru>
2015-08-04 09:17:28 +0300

    new

--- jquery.barcode-scanner-detector.js
+++ jquery.barcode-scanner-detector.js
@@ -1,70 +1,75 @@
 (function ($) {
 
-    function BarcodeScannerDetector(element, options) {
+    function BarcodeScannerDetector(element, options, cb) {
 
         this.element = element || window;
-
-        this.options = {
+        this.callback = cb;
+        this.options = $.extend({
             keypressDelay: 100, // delay between pressing
             charsLength: 10 // >= 10 digits
-        };
-
-        if (options) {
-            this.options = $.extend(this.options, options)
-        }
-
+        }, options);
         this.chars = []; // barcode result
         this.events();
 
     }
 
-    BarcodeScannerDetector.prototype.delay = function (callback, ms) {
+    BarcodeScannerDetector.prototype = {
+        delay: function (callback, ms) {
 
-        clearTimeout(this.timer);
-        this.timer = setTimeout(callback, ms);
+            clearTimeout(this.timer);
+            this.timer = setTimeout(callback, ms);
 
-    };
+        },
 
-    BarcodeScannerDetector.prototype.events = function () {
+        events: function () {
 
-        var _this = this;
+            var _this = this;
 
-        this.element.onkeypress = function (e) {
-            _this.handleKeypress(e);
-        };
+            this.element.onkeypress = function (e) {
+                _this.handleKeypress(e);
+            };
 
-    };
+        },
 
-    BarcodeScannerDetector.prototype.handleKeypress = function (e) {
+        handleKeypress: function (e) {
 
-        var _this = this;
+            var _this = this;
 
-        if (e.which >= 48 && e.which <= 57) {
+            if (e.which >= 48 && e.which <= 57) {
 
-            this.chars.push(String.fromCharCode(e.which));
+                this.chars.push(String.fromCharCode(e.which));
 
-            this.delay(function () {
+                this.delay(function () {
 
-                if (_this.chars.length >= 10) {
+                    var val;
 
-                    if (this.element === window) {
-                        console.log("Barcode scanned: " + _this.chars.join(""));
-                    } else {
-                        this.element.val(_this.chars.join(""));
+                    if (_this.chars.length >= 10) {
+
+                        val = _this.chars.join("");
+
+                        if (_this.callback) {
+                            _this.callback(val)
+                        }
+
                     }
 
-                }
+                    _this.chars = [];
 
-                _this.chars = [];
+                }, this.options.keypressDelay);
+            }
 
-            }, this.options.keypressDelay);
         }
-
     };
 
-    $.fn['BarcodeScannerDetector'] = function (options) {
+
+    $.fn['BarcodeScannerDetector'] = function (options, cb) {
+
+        if ($.isFunction(arguments[0])) {
+            cb = arguments[0];
+        }
+
         return this.each(function () {
-            $.data(this, 'BarcodeScannerDetector', new BarcodeScannerDetector(this, options))
+            $.data(this, 'BarcodeScannerDetector', new BarcodeScannerDetector(this, options, cb))
         });
     }
 
